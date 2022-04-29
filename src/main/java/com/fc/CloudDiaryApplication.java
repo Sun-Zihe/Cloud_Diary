2c72dc11082e11098d484076d7da2ab425351976
SZH <3143350771@qq.com>
2022-04-29 12:59:45 +0800

    云日记Commit-one

---
+++ src/main/java/com/fc/CloudDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDiaryApplication.class, args);
+    }
+
+}
